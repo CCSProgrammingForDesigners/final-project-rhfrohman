6ff84b078b262c4d5b160edf854e4604a0f25dee
Roland Frohman III <rhfrohman@gmail.com>
2018-12-04 16:37:20 -0500

    1st Iteration
    First commit for Final Project

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+//lottie for after effects and motion graphics
+//anime.js
+
+
+//Find and fetch a trivia api
+
+//Create an array of objects that house questions and multiple choice answers
+/* {
+    title: xxxxx
+    question: xxxxx
+    answer: xxxxx
+    choices: {xxxxx,xxxx}
+} 
+
+displayTrivia = () => {
+onNewQuestionLoad = changeColors
+
+//make a timer function
+startTimer();
+//function to timeout out question if not answered
+}
+
+
+
+*/
+
+
+/* //Use a tracking library to actuate process and track hand motions
+
+trackHands();
+
+startTracker();
+
+//Generate random trivia and display in HTML
+generateTrivia(); */
